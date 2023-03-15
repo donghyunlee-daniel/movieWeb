7a0b798c1dd1abe8c8d7de266a04c1994bd90eb4
DongHyun(Daniel) <125529128+donghyunlee-daniel@users.noreply.github.com>
2023-03-15 18:32:12 -0400

    Added buttons and css

--- movieweb-clone/src/App.js
+++ movieweb-clone/src/App.js
@@ -1,7 +1,25 @@
-
+import Button from "./Button";
+import styles from './App.module.css';
+import { useState, useEffect } from "react";
 function App() {
+  const [counter,setValue] =useState(0);
+  const [keyword,setKeyword] = useState("");
+  const onCLick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  }
+  useEffect(iRunOnlyOnce,[]);
   return (
-    <div><h1>Welcome Back!</h1></div>
+    <div>
+      <input value={keyword} 
+      onChange={onChange} 
+      type="text" 
+      placeholder="Seach here..."/>
+      <h1>{counter}</h1>
+      <button onClick={onCLick}>Click me</button>
+      </div>
   );
 }
 
